9ff212a0a044c91e9e634ddb29b9952f0dd5536d
David Urbina <davidcurbina@gmail.com>
2017-11-04 17:51:51 -0400

    teaked google strategy

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: keys.uri
+      callbackURL: '/auth/google/callback',
+      proxy: true
     }, 
     (accessToken, refreshToken, profile, done) =>{
       User.findOne({ googleId:profile.id })
