18c98440dd4bcfa00257b23d68aabcef5fc7a5d8
David Urbina <davidcurbina@gmail.com>
2017-11-13 21:20:38 -0500

    Added heroku build scripts

--- index.js
+++ index.js
@@ -26,5 +26,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+//Route React client properly in prod
+if(process.env.NODE_ENV = 'production'){
+  //Load js and css files
+  app.use(express.static('client/build'));
+  
+  const path = require('path');
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
